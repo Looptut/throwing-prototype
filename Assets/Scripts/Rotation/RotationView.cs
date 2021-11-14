952bdb863fcade06e61cdebc7dcaba5780fee719
Alina Svintsova <aline.svintsova@gmail.com>
2021-11-14 23:14:06 +0700

    little fixes and summary

--- Assets/Scripts/Rotation/RotationView.cs
+++ Assets/Scripts/Rotation/RotationView.cs
@@ -1,23 +1,21 @@
 ﻿using UnityEngine;
-using System;
 /// <summary>
 /// View поворота 
 /// </summary>
 public class RotationView : MonoBehaviour
 {
     [SerializeField] private Rotator rotator;
-    private Vector3 rotation = Vector3.zero;
 
     private void Start()
     {
         rotator.OnChangeDegree += ChangeDegree;
     }
-    private float diff;
+
     private void ChangeDegree(float degree)
     {
-        rotation = transform.rotation.eulerAngles;
         transform.Rotate(Vector3.forward * degree);
     }
+
     private void OnDestroy()
     {
         rotator.OnChangeDegree -= ChangeDegree;
